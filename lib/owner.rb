b671131750e90f584b299372b4fb2da0c4dfc32c
David Foote <david.a.foote@gmail.com>
2019-07-15 18:53:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,9 +54,7 @@ class Owner
   end
   
   def sell_pets
-    @cats.each {|cat, owner| cat.mood = "nervous"}
-    @dogs.each {|dog| dog.mood = "nervous"}
-    @cats.each {|cat| cat.owner = nil}
-    @dogs.each {|dog| dog.owner = nil}
+    @cats.each {|cat, owner| cat.mood = "nervous", cat.owner = nil}
+    @dogs.each {|dog| dog.mood = "nervous", dog.owner = nil}
   end
 end
