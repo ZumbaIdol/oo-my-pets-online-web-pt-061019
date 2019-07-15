e4df04b731c0a9a8993572a78398be13ae422387
David Foote <david.a.foote@gmail.com>
2019-07-15 17:23:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,5 +57,6 @@ class Owner
     @cats.each {|cat| cat.mood = "nervous"}
     @dogs.each {|dog| dog.mood = "nervous"}
     @cats.each {|cat| cat.owner = nil}
+    @dogs.each {|dog| dog.owner = nil}
   end
 end
